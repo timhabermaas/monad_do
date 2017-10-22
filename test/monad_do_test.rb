82f3906b852a25cb924f978c03ec8d7940e0abb1
Tim Habermaas <tim.habermaas@gmail.com>
2017-10-23 00:15:03 +0200

    Add list monad test/example

--- test/monad_do_test.rb
+++ test/monad_do_test.rb
@@ -1,5 +1,6 @@
 require "test_helper"
 require_relative "maybe"
+require_relative "list"
 
 class MonadDoTest < Minitest::Test
   def test_that_it_has_a_version_number
@@ -33,5 +34,15 @@ class MonadDoTest < Minitest::Test
     end
 
     assert_equal Just.new(8), result
+  end
+
+  def test_list_monad
+    result = MonadDo.do(List) do
+      x { List.new([1,2,3]) }
+      y { List.new([4,5,6]) }
+      pure { |x, y| [x, y] }
+    end
+
+    assert_equal [[1,4],[1,5],[1,6],[2,4],[2,5],[2,6],[3,4],[3,5],[3,6]], result.to_a
   end
 end
